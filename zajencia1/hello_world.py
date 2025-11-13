9ac6e50cdea30c4f6e745eec140b4767ab6117c8
L S <lesiaslepko@gmail.com>
2025-11-13 17:01:33 +0100

    zajiencia1

---
+++ zajencia1/hello_world.py
@@ -0,0 +1,30 @@
+print("hello_world")
+
+################  zmienne liczbowe
+
+wiek = 30 # typ indijr
+
+print(wiek)
+
+wiek = 20 + 5
+
+wiek2 = 20 - 5
+
+wiek3 = 5 * 2
+
+wiek4 = 140 / 5  # typ float
+
+kwadrat_wieku = 5**2
+
+cena_jabka = 2,99(       )
+
+modulo = 6 % 4  # reszta z dielenia
+
+
+print(wiek)
+print(wiek2)
+print(wiek3)
+print(wiek4)
+print(cena_jabka)
+print(kwadrat_wieku)
+print(modulo)
